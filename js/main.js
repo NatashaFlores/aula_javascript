11443da80f22f185b7d1069bf29da47cf94de9c4
NatashaFlores <natasha_ksf@outlook.com>
2020-06-04 11:32:28 -0300

    class: first class plus second class

--- js/main.js
+++ js/main.js
@@ -4,8 +4,8 @@ var nome = "Natasha";
 var idade = 29;
 alert(nome + " tem " + idade + " anos");
 
-var idade2 = 10;
-alert(idade + idade2); 
+var idade2 = "10";
+alert(idade + idade2 + "-"); 
 
 var frase = "Japao é o melhor time do mundo.";
 console.log(frase.replace("Japao", "Brasil"));
@@ -75,3 +75,74 @@ console.log(d.getMonth()+1);
 console.log(d.getMinutes());
 console.log(d.getHours());
 
+// Funções
+
+function soma(n1,n2)
+{
+    return n1+n2;
+}
+alert(soma(5,10));
+
+function setReplace(frase, nome, novo_nome){
+    return frase.replace(nome, novo_nome);
+}
+
+alert(setReplace("Vai Japao","Japao", "Brasil"));
+
+function validaIdade(idade)
+{
+    var validar;
+    if(idade >= 18){
+        validar = true;
+    }else{
+        validar = false;
+    }
+    return validar;
+}
+
+var idade = prompt("Qual sua idade?");
+console.log(validaIdade(idade));
+
+
+//Manipulando eventos de página
+
+function clicou(){
+    //alert("Obrigada por clicar.");
+    //console.log(document.getElementById("agradecimento"));
+
+    //Para ID
+    document.getElementById("agradecimento").innerHTML = "<b>Obrigada por clicar</b>"; 
+    
+}
+
+function redirecionar(){
+    // Abre uma aba nova
+    window.open("https://translate.google.com/"); 
+    // Carrega a página na mesma aba
+    //window.location.href = "https://translate.google.com/"; 
+}
+
+function trocar(elemento){
+    //Pode fazer sem passar parametro
+    //document.getElementById("mousemove").innerHTML = "Obrigada por passar o mouse";
+
+    //Sabe qual é o elemento com o this.
+    elemento.innerHTML = "Obrigada por passar o mouse";
+}
+
+function voltar(elemento){
+    //Pode fazer sem passar parametro
+    //document.getElementById("mousemove").innerHTML = "Passe o mouse aqui";
+
+    //Sabe qual é o elemento com o this.
+    elemento.innerHTML = "Passe o mouse aqui";
+}
+
+function load(){
+    alert("Página carregada");
+}
+
+function funcaoChange(elemento){
+    // Troca o elemento
+    console.log(elemento.value);
+}
